d1009fed605dd8eb93fa4b0b94ff189ea9446ea3
Viikuu <viiktorw77@gmail.com>
2022-11-10 22:23:15 +0100

    Create Storage module to write and read data from json file

---
+++ controllers/storage.mjs
@@ -0,0 +1,21 @@
+import { readFile, writeFile, access, mkdir } from 'fs/promises';
+
+async function Storage (fileName) {
+	fileName = `../data/${fileName}`;
+	try {
+		await access(fileName);
+	} catch {
+		await mkdir(fileName);
+	}
+
+	async function saveData(data) {
+		await writeFile(fileName, JSON.stringify(data, undefined, '  '), { encoding: 'utf-8' });
+	}
+
+	async function getData() {
+		const fileContent = await readFile(fileName, { encoding: 'utf-8' });
+		return JSON.parse(fileContent);
+	}
+}
+
+export default Storage;
