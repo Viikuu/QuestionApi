05aae52c1d7e79a11415fe7a6d134a4220e7837f
Viikuu <viiktorw77@gmail.com>
2022-11-12 15:10:28 +0100

    Tests file creation by Storage module

---
+++ test/storage.test.mjs
@@ -0,0 +1,22 @@
+import test from 'ava';
+import Storage from '../controllers/storage.mjs';
+import { access, unlink } from 'fs/promises';
+import { faker } from '@faker-js/faker';
+
+test('Should throw error if fileName is not a json file name', async t => {
+	await t.throwsAsync(async () => {
+		await Storage('123');
+	}, {instanceOf:Error, message: 'Specified file should be a JSON file'});
+});
+
+test('Should create data/filename if data/filename does not exist', async t => {
+	const filename = `${faker.datatype.string()}.json`;
+	await Storage(filename);
+	await t.notThrowsAsync(async () => {
+		await access(`data/${filename}`);
+		await unlink(`data/${filename}`);
+	});
+
+});
+
+
