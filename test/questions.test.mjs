3183137cf39ce760dea8dd30deb0b629cd14420a
Viikuu <viiktorw77@gmail.com>
2022-11-14 22:06:40 +0100

    Style questions.test.mjs using xo rules

--- test/questions.test.mjs
+++ test/questions.test.mjs
@@ -1,15 +1,15 @@
-import {unlink, writeFile} from 'fs/promises';
+import {unlink, writeFile} from 'node:fs/promises';
 import test from 'ava';
-import { faker } from '@faker-js/faker';
+import {faker} from '@faker-js/faker';
 import Questions from '../controllers/questions.mjs';
-
-test.beforeEach(async () => {
-	await writeFile('data/test.json', JSON.stringify([], undefined, '  '), { encoding: 'utf-8' });
-})
 
 test.after(async () => {
 	await unlink('data/test.json');
-})
+});
+
+test.beforeEach(async () => {
+	await writeFile('data/test.json', JSON.stringify([], undefined, '  '), {encoding: 'utf8'});
+});
 
 test.serial('Should return empty list when no questions were created', async t => {
 	t.deepEqual(await (await Questions('test.json')).getQuestions(), []);
@@ -21,16 +21,16 @@ test.serial('Should return list of questions which are in a test.json file', asy
 			id: 'e6455abf-22f9-4a9a-a942-b0fe9d848116',
 			summary: 'What is my name?',
 			author: 'Jack London',
-			answers: []
+			answers: [],
 		},
 		{
 			id: '35c05570-622e-4008-a389-3694873e667a',
 			summary: 'Who are you?',
 			author: 'Tim Doods',
-			answers: []
-		}
+			answers: [],
+		},
 	];
-	await writeFile('data/test.json', JSON.stringify(testQuestions, undefined, '  '), { encoding: 'utf-8' });
+	await writeFile('data/test.json', JSON.stringify(testQuestions, undefined, '  '), {encoding: 'utf8'});
 
 	t.deepEqual(await (await Questions('test.json')).getQuestions(), testQuestions);
 });
@@ -41,21 +41,21 @@ test.serial('Should return undefined when question with specified id does not ex
 			id: faker.datatype.uuid(),
 			summary: 'What is my name?',
 			author: 'Jack London',
-			answers: []
+			answers: [],
 		},
 		{
 			id: faker.datatype.uuid(),
 			summary: 'Who are you?',
 			author: 'Tim Doods',
-			answers: []
-		}
+			answers: [],
+		},
 	];
 
-	t.deepEqual(await (await Questions('test.json')).getQuestionById(faker.datatype.uuid()), undefined);
+	t.is(await (await Questions('test.json')).getQuestionById(faker.datatype.uuid()), undefined);
 
-	await writeFile('data/test.json', JSON.stringify(testQuestions, undefined, '  '), { encoding: 'utf-8' });
+	await writeFile('data/test.json', JSON.stringify(testQuestions, undefined, '  '), {encoding: 'utf8'});
 
-	t.deepEqual(await (await Questions('test.json')).getQuestionById(faker.datatype.uuid()), undefined);
+	t.is(await (await Questions('test.json')).getQuestionById(faker.datatype.uuid()), undefined);
 });
 
 test.serial('Should return question with specified id ', async t => {
@@ -64,49 +64,48 @@ test.serial('Should return question with specified id ', async t => {
 			id: 'e6455abf-22f9-4a9a-a942-b0fe9d848116',
 			summary: 'What is my name?',
 			author: 'Jack London',
-			answers: []
+			answers: [],
 		},
 		{
 			id: '35c05570-622e-4008-a389-3694873e667a',
 			summary: 'Who are you?',
 			author: 'Tim Doods',
-			answers: []
-		}
+			answers: [],
+		},
 	];
-	await writeFile('data/test.json', JSON.stringify(testQuestions, undefined, '  '), { encoding: 'utf-8' });
+	await writeFile('data/test.json', JSON.stringify(testQuestions, undefined, '  '), {encoding: 'utf8'});
 
 	t.deepEqual(await (await Questions('test.json')).getQuestionById('e6455abf-22f9-4a9a-a942-b0fe9d848116'), testQuestions[0]);
 	t.deepEqual(await (await Questions('test.json')).getQuestionById('35c05570-622e-4008-a389-3694873e667a'), testQuestions[1]);
 });
 
 test.serial('Should throw error if added question is not typeof object ', async t => {
-	let question = "123";
+	let question = '123';
 	await t.throwsAsync(async () => {
 		await (await Questions('test.json')).addQuestion(question);
-	}, {instanceOf:Error, message: `Expected question to be an object, got ${typeof question}`});
+	}, {instanceOf: Error, message: `Expected question to be an object, got ${typeof question}`});
 
 	question = 123;
 	await t.throwsAsync(async () => {
 		await (await Questions('test.json')).addQuestion(question);
-	}, {instanceOf:Error, message: `Expected question to be an object, got ${typeof question}`});
+	}, {instanceOf: Error, message: `Expected question to be an object, got ${typeof question}`});
 
 	question = [];
 	await t.throwsAsync(async () => {
 		await (await Questions('test.json')).addQuestion(question);
-	}, {instanceOf:Error, message: `Expected question to be an object, got ${typeof question}`});
-
+	}, {instanceOf: Error, message: `Expected question to be an object, got ${typeof question}`});
 });
 
 test.serial('Should throw error if added question.author is not typeof string ', async t => {
 	let question = {
 		id: 'e6455abf-22f9-4a9a-a942-b0fe9d848116',
 		summary: 'What is my name?',
-		author: {name:'Jack London'},
+		author: {name: 'Jack London'},
 		answers: [],
 	};
 	await t.throwsAsync(async () => {
 		await (await Questions('test.json')).addQuestion(question);
-	}, {instanceOf:Error, message: `Expected question.author to be a string, got ${typeof question.author}`});
+	}, {instanceOf: Error, message: `Expected question.author to be a string, got ${typeof question.author}`});
 
 	question = {
 		id: 'e6455abf-22f9-4a9a-a942-b0fe9d848116',
@@ -116,7 +115,7 @@ test.serial('Should throw error if added question.author is not typeof string ',
 	};
 	await t.throwsAsync(async () => {
 		await (await Questions('test.json')).addQuestion(question);
-	}, {instanceOf:Error, message: `Expected question.author to be a string, got ${typeof question.author}`});
+	}, {instanceOf: Error, message: `Expected question.author to be a string, got ${typeof question.author}`});
 
 	question = {
 		id: 'e6455abf-22f9-4a9a-a942-b0fe9d848116',
@@ -125,7 +124,7 @@ test.serial('Should throw error if added question.author is not typeof string ',
 	};
 	await t.throwsAsync(async () => {
 		await (await Questions('test.json')).addQuestion(question);
-	}, {instanceOf:Error, message: `Expected question.author to be a string, got ${typeof question.author}`});
+	}, {instanceOf: Error, message: `Expected question.author to be a string, got ${typeof question.author}`});
 
 	question = {
 		id: 'e6455abf-22f9-4a9a-a942-b0fe9d848116',
@@ -135,5 +134,5 @@ test.serial('Should throw error if added question.author is not typeof string ',
 	};
 	await t.throwsAsync(async () => {
 		await (await Questions('test.json')).addQuestion(question);
-	}, {instanceOf:Error, message: `Expected question.author to be a string, got ${typeof question.author}`});
+	}, {instanceOf: Error, message: `Expected question.author to be a string, got ${typeof question.author}`});
 });
