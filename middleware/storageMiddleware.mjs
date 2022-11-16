9289083da0b96469b41411d39405063576a386f8
Viikuu <viiktorw77@gmail.com>
2022-11-16 22:30:00 +0100

    storage-middleware.mjs refactor with xo rules and name change from storageMiddleware.mjs

--- middleware/storageMiddleware.mjs
+++
@@ -1,6 +0,0 @@
-import Questions from '../controllers/questions.mjs';
-
-export default (fileName) => async (req, res, next) => {
-	req = {...req, questionsRepo: await Questions(fileName) };
-	next()
-}
