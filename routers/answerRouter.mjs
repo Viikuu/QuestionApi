3f74e5e6bcfbf5976debc5e5e27a6677baebca9c
Viikuu <viiktorw77@gmail.com>
2022-11-20 11:23:05 +0100

    feat: add get answers endpoint

--- routers/answerRouter.mjs
+++ routers/answerRouter.mjs
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express.Router();
 
-app.get('/:questionId/answers', async (req, res) => {
-
+app.get('/:questionId/answers', async (request, response, next) => {
+	const { questionId } = request.params;
+	try{
+		const answers = await request.questionsRepo.getAnswers(questionId);
+		response.json({ success: true, answers });
+	} catch (error) {
+		next(error);
+	}
 })
 
 app.post('/:questionId/answers',async (req, res) => {
