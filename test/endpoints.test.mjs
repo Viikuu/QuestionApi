7f328a80854002c965e28506a38c368521ff1f7e
Viikuu <viiktorw77@gmail.com>
2022-11-21 20:49:22 +0100

    feat: post /questions test

--- test/endpoints.test.mjs
+++ test/endpoints.test.mjs
@@ -25,3 +25,15 @@ test.serial('get /questions empty questionRepo', async t => {
 	const message = await got('questions/', {prefixUrl: t.context.prefixUrl}).json();
 	t.deepEqual(message, {success: true, questions: []});
 });
+
+test.serial('post /questions', async t => {
+	const question = {
+		id: 'e6455abf-22f9-4a9a-a942-b0fe9d848116',
+		summary: 'What is my name?',
+		author: 'Jack London',
+		answers: [],
+	};
+	await t.notThrowsAsync(async () => {
+		await got.post('questions/', {prefixUrl: t.context.prefixUrl, json: question});
+	});
+});
