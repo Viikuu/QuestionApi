9d8243df4126a3e7cfa53fc03719a684f8153a37
Viikuu <viiktorw77@gmail.com>
2022-11-21 19:54:43 +0100

    refactor: refactor main server code to make it testable

--- app.mjs
+++ app.mjs
@@ -2,8 +2,8 @@ import express from 'express';
 import questionStorage from './middleware/storage-middleware.mjs';
 import {questionRouter} from './routers/question-router.mjs';
 
-export function app_init(STORAGE_FILE_PATH) {
-	const PORT = 3000;
+function app_init(STORAGE_FILE_PATH) {
+
 
 	const app = express();
 
@@ -13,14 +13,11 @@ export function app_init(STORAGE_FILE_PATH) {
 
 	app.use('/questions', questionRouter);
 
-
 	app.get('/', (_, res) => {
 		res.json({ message: 'Welcome to responder!' });
 	});
 
-	app.listen(PORT, async () => {
-		console.log(`Responder app listening on port ${PORT}`);
-	});
-
 	return app;
-};
+}
+
+export default app_init;
--- index.mjs
+++ index.mjs
@@ -1,5 +1,10 @@
-import {app_init} from './app.mjs';
+import app_init from './app.mjs';
 
 const STORAGE_FILE_PATH = 'questions.json';
 
 const app = app_init(STORAGE_FILE_PATH);
+const PORT = 3000;
+
+app.listen(PORT, async () => {
+	console.log(`Responder app listening on port ${PORT}`);
+});
