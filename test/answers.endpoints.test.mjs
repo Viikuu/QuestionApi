c2dc7f7598627122575b8df2aef36b9a4a607984
Viikuu <viiktorw77@gmail.com>
2022-11-22 14:38:49 +0100

    feat: get answer by Id test

--- test/answers.endpoints.test.mjs
+++ test/answers.endpoints.test.mjs
@@ -76,3 +76,34 @@ test.serial('post /questions/:questionId/answers', async t => {
 		await got.post('questions/e6455abf-22f9-4a9a-a942-b0fe9d848116/answers', {prefixUrl: t.context.prefixUrl, json: answer});
 	});
 });
+
+test.serial('get /questions/:questionId/answers/:answerId', async t => {
+	const answer = {
+		id: 'f4325fw4-22f9-4a9a-a942-b0fe9d848116',
+		author: 'Dr Strange',
+		summary: 'It is egg-shaped.',
+	};
+
+	const testQuestions = [
+		{
+			id: 'e6455abf-22f9-4a9a-a942-b0fe9d848116',
+			summary: 'What is my name?',
+			author: 'Jack London',
+			answers: [answer],
+		},
+		{
+			id: '35c05570-622e-4008-a389-3694873e667a',
+			summary: 'Who are you?',
+			author: 'Tim Doods',
+			answers: [],
+		},
+	];
+
+	await writeFile('data/test4.json', JSON.stringify(testQuestions, undefined, '  '), {encoding: 'utf8'});
+
+	await t.notThrowsAsync(async () => {
+		const message = await got('questions/e6455abf-22f9-4a9a-a942-b0fe9d848116/answers/f4325fw4-22f9-4a9a-a942-b0fe9d848116', {prefixUrl: t.context.prefixUrl}).json();
+		t.deepEqual(message, {success: true, answer: answer})
+	});
+});
+
