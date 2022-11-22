6af5041b575c84b68de4c63cfef743acd75db88d
Viikuu <viiktorw77@gmail.com>
2022-11-22 13:01:52 +0100

    feat: get questions/:questionId main func test

--- test/endpoints.test.mjs
+++ test/endpoints.test.mjs
@@ -3,8 +3,7 @@ import test from 'ava';
 import got from 'got';
 import listen from 'test-listen';
 import app_init from '../app.mjs';
-import {unlink} from 'node:fs/promises';
-
+import {unlink, writeFile} from 'node:fs/promises';
 test.before(async t => {
 	const app = app_init('test3.json')
 	t.context.server = http.createServer(app);
@@ -36,4 +35,35 @@ test.serial('post /questions', async t => {
 	await t.notThrowsAsync(async () => {
 		await got.post('questions/', {prefixUrl: t.context.prefixUrl, json: question});
 	});
+});
+
+test.serial('get /questions/:questionId', async t => {
+	const testQuestions = [
+		{
+			id: 'e6455abf-22f9-4a9a-a942-b0fe9d848116',
+			summary: 'What is my name?',
+			author: 'Jack London',
+			answers: [],
+		},
+		{
+			id: '35c05570-622e-4008-a389-3694873e667a',
+			summary: 'Who are you?',
+			author: 'Tim Doods',
+			answers: [],
+		},
+	];
+
+	await writeFile('data/test3.json', JSON.stringify(testQuestions, undefined, '  '), {encoding: 'utf8'});
+
+
+	await t.notThrowsAsync(async () => {
+		const question = await got('questions/e6455abf-22f9-4a9a-a942-b0fe9d848116', {prefixUrl: t.context.prefixUrl}).json();
+		t.deepEqual(question, {success: true, question: testQuestions[0]});
+	});
+
+	await t.notThrowsAsync(async () => {
+		const question = await got('questions/35c05570-622e-4008-a389-3694873e667a', {prefixUrl: t.context.prefixUrl}).json();
+		t.deepEqual(question, {success: true, question: testQuestions[1]});
+	});
+
 });
