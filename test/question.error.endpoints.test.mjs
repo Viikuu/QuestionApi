1905a6d75dbde209aaf4ceffe8424e5ea34aa5a3
Viikuu <viiktorw77@gmail.com>
2022-11-24 20:50:18 +0100

    feat: new get question by id errors test

--- test/question.error.endpoints.test.mjs
+++ test/question.error.endpoints.test.mjs
@@ -4,6 +4,7 @@ import got from 'got';
 import listen from 'test-listen';
 import app_init from '../app.mjs';
 import {unlink, writeFile} from 'node:fs/promises';
+import {faker} from '@faker-js/faker';
 
 test.before(async t => {
 	const app = app_init('testError.json')
@@ -45,3 +46,57 @@ test.serial('post /questions error', async t => {
 		'Expected question to be an object, got object');
 });
 
+test.serial('get /questions/:questionId error', async t => {
+
+	const test = async (code, message, uuid) => {
+		try {
+			const response = await got(`questions/${uuid}`, {
+				prefixUrl: t.context.prefixUrl,
+				retry: {
+					limit: 0
+				},
+			});
+		} catch (error) {
+			t.is(error.response.statusCode, code);
+			t.deepEqual(JSON.parse(error.response.body), {success: false, message});
+		}
+	}
+
+	await test(404,
+		'Question with specified id does not exist',
+		faker.datatype.uuid());
+
+	let testQuestions = [
+		{
+			id: 'e6455abf-22f9-4a9a-a942-b0fe9d848116',
+			summary: 'What is my name?',
+			author: 'Jack London',
+			answers: [],
+		},
+		{
+			id: '35c05570-622e-4008-a389-3694873e667a',
+			summary: 'Who are you?',
+			author: 'Tim Doods',
+			answers: [],
+		},
+	];
+
+	await writeFile('data/testError.json', JSON.stringify(testQuestions, undefined, '  '), {encoding: 'utf8'});
+
+	await test(404,
+		'Question with specified id does not exist',
+		faker.datatype.uuid());
+
+	const id = 'e6455abf-22f9-4a9a-a942-b0fe9d848116';
+
+	await writeFile('data/testError.json', JSON.stringify([
+		{
+			id,
+		},
+	], undefined, '  '), {encoding: 'utf8'});
+
+	await test(500,
+		`Question object damaged!`,
+		id);
+
+});
