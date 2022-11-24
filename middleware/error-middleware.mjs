821e4214902f5667f59ba81e3d82133de0c7ae95
Viikuu <viiktorw77@gmail.com>
2022-11-24 15:24:13 +0100

    reformat: express errorHandler rewrite

--- middleware/error-middleware.mjs
+++ middleware/error-middleware.mjs
@@ -1,12 +1,23 @@
 import ApiError from '../utils/customError.mjs';
 
-export default function errorHandler (error, request, response, next) {
+export default function errorHandler(error, request, response, next) {
 	if (response.headersSent) {
 		return next(error);
 	}
+	const arr = [
+		'Answer with specified id does not exist',
+		`Question with specified id does not exist`,
+	];
 	if (error instanceof ApiError) {
-		response.status(500).json({success: false, message: error.message })
+		if (arr.includes(error.message)) {
+			response.status(404);
+		}else if (error.message === `Question object damaged!`) {
+			response.status(500);
+		} else {
+			response.status(400);
+		}
+		response.json({success: false, message: error.message});
 		return;
 	}
-	response.status(500).send({ error: 'Something failed!' })
+	response.status(500).send({error: 'Something failed!'});
 };
