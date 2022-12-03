da61f4f2d53212c1dc57ac3145a22c8ab770b8a5
Viikuu <viiktorw77@gmail.com>
2022-12-03 16:02:56 +0100

    Revert "feat: turn on dotenv config"
    This reverts commit 9079f90acda8d9080d86e2df1799fc2659e78d93.

--- index.mjs
+++ index.mjs
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv';
 import appInit from './app.mjs';
 
 const STORAGE_FILE_PATH = 'questions.json';
-
-dotenv.config();
 
 const app = appInit(STORAGE_FILE_PATH);
 const PORT = 3000;
